824557c749a9d857758ff8e52a71766e1adf938d
Shobi P P <ppshobi@gmail.com>
2017-07-30 14:23:43 +0530

    Tasks Model

---
+++ api/models/toDoListModel.js
@@ -0,0 +1,25 @@
+'use strict'
+var mongoose = require('mongoose');
+var schema = mongoose.Schema;
+
+var TaskSchema = new schema({
+    name : {
+        type : string,
+        Required : "Kindly enter a name for your task"
+    },
+
+    created_data :{
+        type : Date,
+        default : Date.now
+    },
+
+    status : {
+        type : [{
+            type : String,
+            enum : ['pending', 'ongoing', 'completed']
+        }],
+        default : ['pending'] 
+    }
+});
+
+module.exports = mongoose.model('Tasks', TaskSchema);
