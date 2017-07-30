370f2af81fb840fb698f16b03c4523c491b8a162
Shobi P P <ppshobi@gmail.com>
2017-07-30 15:23:21 +0530

    setup to connect with mongoose

--- server.js
+++ server.js
@@ -1,7 +1,21 @@
 var express = require('express'),
     app = express(),
-    port = process.env.port || 3000;
+    port = process.env.port || 3000,
+    mongoose = require('mongoose'),
+    Task = require('./api/models/toDoListModel'),
+    bodyParser = require('body-parser');
+
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://localhost/ToDoDb');
+
+app.use(bodyParser.urlencoded({ extended:true }));
+app.use(bodyParser.json());
+
+var routes = require('./api/routes/toDoListRoutes');
+
+routes(app);
 
 app.listen(port)
+
 
 console.log("ToDo List Api Server Started on Port " + port);
