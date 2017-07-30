b284674de5ff4838ef0dffdd1f571a9a419577c9
Shobi P P <ppshobi@gmail.com>
2017-07-30 15:37:06 +0530

    implemented read, update, delete methods

--- api/controllers/toDoListController.js
+++ api/controllers/toDoListController.js
@@ -8,6 +8,7 @@ exports.listTasks = function(req , res){
         if(err){
             res.send(err);
         }
+        
         res.json(tasks);
     });
 };
@@ -18,6 +19,39 @@ exports.createTask = function(req, res){
         if(err){
             res.send(err);
         }
+
         res.json(task);
+    });
+};
+
+exports.listTask = function(req, res){
+    Task.findById(req.params.taskId, function(err, task){
+        if(err){
+            res.send(err);
+        }
+
+        res.json(task);
+    });
+};
+
+exports.updateTask = function(req, res){
+    Task.findOneAndUpdate({_id: req.params.taskId}, req.body, {new:true}, function(err, task){
+        if(err){
+            res.send(err)
+        }
+
+        res.json(task);
+    });
+};
+
+exports.deleteTask = function(req, res){
+    Task.remove({_id:req.params.taskId}, function(err, task){
+        if(err){
+            res.send(err);
+        }
+
+        res.json({
+            message: 'Task Successfully Deleted'
+        })
     });
 };
